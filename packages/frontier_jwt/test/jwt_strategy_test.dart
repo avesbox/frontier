875d9746e74d44cef86c879fe594e3c6313f3b1d
Francesco Vallone <vallonefrancesco587@gmail.com>
2024-10-19 11:00:19 +0200

    feat(jwt): add tests and options

--- packages/frontier_jwt/test/jwt_strategy_test.dart
+++ packages/frontier_jwt/test/jwt_strategy_test.dart
@@ -1,8 +1,25 @@
-import 'package:jwt_strategy/jwt_strategy.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:test/test.dart';
+import 'package:frontier_jwt/jwt_strategy.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('$JwtStrategy', () {
+    test('should have a name of jwt', () {
+      final strategy = JwtStrategy(JwtStrategyOptions(secret: SecretKey('')));
+      expect(strategy.name, 'jwt');
+    });
+
+    test('should authenticate a token', () async {
+      final strategy = JwtStrategy(JwtStrategyOptions(secret: SecretKey('hello')));
+      final token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.ElsKKULlzGtesThefMuj2_a6KIY9L5i2zDrBLHV-e0M';
+      final jwt = await strategy.authenticate(token);
+      expect(jwt, isA<JWT>());
+    });
+
+    test('should throw an error if the token is invalid', () async {
+      final strategy = JwtStrategy(JwtStrategyOptions(secret: SecretKey('hella')));
+      final token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.ElsKKULlzGtesThefMuj2_a6KIY9L5i2zDrBLHV-e0M';
+      expect(strategy.authenticate(token), throwsA(isA<JWTException>()));
+    });
   });
 }
