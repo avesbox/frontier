f47d8a8f98e1ed55924bc505ee00534de5df8c89
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-02-12 00:14:44 +0100

    fix: fix to readme

--- packages/frontier_basic/lib/frontier_basic.dart
+++ packages/frontier_basic/lib/frontier_basic.dart
@@ -5,25 +5,21 @@ import 'dart:convert';
 import 'package:frontier_strategy/frontier_strategy.dart';
 
 final class BasicAuthOptions extends StrategyOptions {
-
   /// The header to look for in the request
-  /// 
+  ///
   /// By default this is set to 'Authorization', but can be changed
   final String header;
 
   /// A fallback header to use if the header is not found
-  /// 
+  ///
   /// By default this is set to 'Proxy-Authorization', but can be changed
   final String proxyHeader;
 
   /// Creates a new BasicAuthOptions
-  BasicAuthOptions(
-    {
-      this.header = 'Authorization',
-      this.proxyHeader = 'Proxy-Authorization',
-    }
-  );
-
+  BasicAuthOptions({
+    this.header = 'Authorization',
+    this.proxyHeader = 'Proxy-Authorization',
+  });
 }
 
 /// The credentials gotten from the header
@@ -39,7 +35,9 @@ final class Credentials {
 
   @override
   bool operator ==(Object other) {
-    return other is Credentials && other.username == username && other.password == password;
+    return other is Credentials &&
+        other.username == username &&
+        other.password == password;
   }
 
   @override
@@ -48,7 +46,6 @@ final class Credentials {
 
 /// The strategy for basic authentication
 class BasicAuthStrategy extends Strategy<BasicAuthOptions> {
-
   /// Creates a new BasicAuthStrategy
   BasicAuthStrategy(super.options, super.callback);
 
@@ -58,12 +55,12 @@ class BasicAuthStrategy extends Strategy<BasicAuthOptions> {
   @override
   Future<void> authenticate(StrategyRequest request) async {
     Credentials? credentials;
-    if(request.headers.containsKey(options.header)) {
+    if (request.headers.containsKey(options.header)) {
       credentials = _decode(request.headers[options.header]!);
-    } else if(request.headers.containsKey(options.proxyHeader)) {
+    } else if (request.headers.containsKey(options.proxyHeader)) {
       credentials = _decode(request.headers[options.proxyHeader]!);
     }
-    if(credentials != null) {
+    if (credentials != null) {
       callback.call(options, credentials, done);
       return;
     }
@@ -73,10 +70,9 @@ class BasicAuthStrategy extends Strategy<BasicAuthOptions> {
   Credentials? _decode(String value) {
     final decoded = utf8.decode(base64.decode(value));
     final parts = decoded.split(':');
-    if(parts.length != 2) {
+    if (parts.length != 2) {
       return null;
     }
     return Credentials(username: parts[0], password: parts[1]);
   }
-  
 }
--- packages/frontier_basic/test/frontier_basic_test.dart
+++ packages/frontier_basic/test/frontier_basic_test.dart
@@ -6,11 +6,13 @@ void main() {
   test('$BasicAuthStrategy', () async {
     final credentials = Credentials(username: 'test', password: 'test');
     final options = BasicAuthOptions();
-    final strategy = BasicAuthStrategy(options, (options, credentials, done) async {
+    final strategy =
+        BasicAuthStrategy(options, (options, credentials, done) async {
       done(credentials);
     });
     expect(strategy.name, 'BasicAuthentication');
-    strategy.authenticate(StrategyRequest(headers: {'Authorization': 'dGVzdDp0ZXN0'}));
+    strategy.authenticate(
+        StrategyRequest(headers: {'Authorization': 'dGVzdDp0ZXN0'}));
     final result = await strategy.future;
     expect(result, equals(credentials));
   });
