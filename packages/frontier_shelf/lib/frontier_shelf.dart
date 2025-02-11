004e94c092371870393324d4f6ee8901072c679c
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-02-12 00:00:42 +0100

    chore: prepare to release frontier_shelf

--- packages/frontier_shelf/lib/frontier_shelf.dart
+++ packages/frontier_shelf/lib/frontier_shelf.dart
@@ -1,21 +1,23 @@
 import 'package:frontier/frontier.dart';
 import 'package:shelf/shelf.dart';
 
+/// The global instance of [Frontier].
 final frontier = Frontier();
 
-Middleware frontierMiddleware(
-  String strategy,
-  Future<StrategyRequest> Function(Request request) dataFromReq,
-  { String unauthorizedMessage = 'The authentication failed!' }
-) {
+/// A middleware that authenticates requests using the given [strategy].
+Middleware frontierMiddleware(String strategy,
+    Future<StrategyRequest> Function(Request request) dataFromReq,
+    {String unauthorizedMessage = 'The authentication failed!'}) {
   return (Handler innerHandler) {
     return (request) async {
       final data = await dataFromReq(request);
       final value = await frontier.authenticate(strategy, data);
-      if(value == null || value == false) {
-        return Response.unauthorized(unauthorizedMessage, context: request.context);
+      if (value == null || value == false) {
+        return Response.unauthorized(unauthorizedMessage,
+            context: request.context);
       }
-      final changedRequest = request.change(context: { 'frontier.$strategy': value });
+      final changedRequest =
+          request.change(context: {'frontier.$strategy': value});
       return await innerHandler.call(changedRequest);
     };
   };
--- packages/frontier_shelf/test/frontier_shelf_test.dart
+++ packages/frontier_shelf/test/frontier_shelf_test.dart
@@ -9,12 +9,10 @@ final class HeaderOptions extends StrategyOptions {
   final String key;
   final String value;
 
-  HeaderOptions(
-      {required this.key, required this.value});
+  HeaderOptions({required this.key, required this.value});
 }
 
 class HeaderStrategy extends Strategy<HeaderOptions> {
-
   HeaderStrategy(super.options, super.callback);
 
   @override
@@ -30,55 +28,50 @@ class HeaderStrategy extends Strategy<HeaderOptions> {
 void main() {
   group('test integration with Shelf', () {
     setUpAll(() {
-      frontier.use(HeaderStrategy(HeaderOptions(key: 'hello', value: 'world'), (options, result, done) async {
+      frontier.use(HeaderStrategy(HeaderOptions(key: 'hello', value: 'world'),
+          (options, result, done) async {
         done(result);
       }));
     });
-    test('if the request contains the right header the request should go through and the context should be modified', () async {
+    test(
+        'if the request contains the right header the request should go through and the context should be modified',
+        () async {
       var handler = const Pipeline()
-        .addMiddleware(frontierMiddleware('Header', (req) async => StrategyRequest(headers: req.headers)))
-        .addHandler((req) => Response.ok('ok!', context: req.context));
-      final req = Request(
-        'get', 
-        clientUri,
-        headers: {
-          'hello': 'world'
-        }
-      );
+          .addMiddleware(frontierMiddleware(
+              'Header', (req) async => StrategyRequest(headers: req.headers)))
+          .addHandler((req) => Response.ok('ok!', context: req.context));
+      final req = Request('get', clientUri, headers: {'hello': 'world'});
       final response = await handler(req);
       expect(response.context['frontier.Header'], equals(true));
       expect(response.statusCode, 200);
       expect(response.context['frontier.Header'], true);
     });
 
-    test('if the request does not contain the right header the request should not go through with a 401 status code', () async {
+    test(
+        'if the request does not contain the right header the request should not go through with a 401 status code',
+        () async {
       var handler = const Pipeline()
-        .addMiddleware(frontierMiddleware('Header', (req) async => StrategyRequest(headers: req.headers)))
-        .addHandler((req) => Response.ok('ok!'));
+          .addMiddleware(frontierMiddleware(
+              'Header', (req) async => StrategyRequest(headers: req.headers)))
+          .addHandler((req) => Response.ok('ok!'));
 
-      final response = await handler(Request(
-        'get', 
-        clientUri,
-        headers: {
-          'hello': ''
-        }
-      ));
+      final response =
+          await handler(Request('get', clientUri, headers: {'hello': ''}));
 
       expect(response.statusCode, 401);
     });
 
-    test('if the request does not contain the right header the request and custom message is passed the request should not go through with the custom message in the response.', () async {
+    test(
+        'if the request does not contain the right header the request and custom message is passed the request should not go through with the custom message in the response.',
+        () async {
       var handler = const Pipeline()
-        .addMiddleware(frontierMiddleware('Header', (req) async => StrategyRequest(headers: req.headers), unauthorizedMessage: 'Custom Message!'))
-        .addHandler((req) => Response.ok('ok!'));
+          .addMiddleware(frontierMiddleware(
+              'Header', (req) async => StrategyRequest(headers: req.headers),
+              unauthorizedMessage: 'Custom Message!'))
+          .addHandler((req) => Response.ok('ok!'));
 
-      final response = await handler(Request(
-        'get', 
-        clientUri,
-        headers: {
-          'hello': ''
-        }
-      ));
+      final response =
+          await handler(Request('get', clientUri, headers: {'hello': ''}));
       final res = await response.readAsString();
       expect(res, 'Custom Message!');
     });
